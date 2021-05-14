14a7ebf0e10655e6ff62030417e35e3968e5b48c
Jinho Myung <jmyung@c02dq8uhmd6r.corp.intusurg.com>
2021-05-13 22:54:34 -0700

    day1 completed

---
+++ 100DaysOfSwift/day1.playground/Contents.swift
@@ -0,0 +1,35 @@
+import UIKit
+
+var str = "Hello, playground"
+str = "Goodbye"
+
+var age = 38
+var population = 8_000_000
+
+var str1 = """
+This goes
+over multiple lines
+"""
+
+var str2 = """
+This goes \
+over multiple \
+lines
+"""
+
+var pi = 3.141
+
+var awesome = true
+
+var score = 85
+var str3 = "Your score was \(score)"
+var results = "The test results are here: \(str3)"
+
+let firstName = "JINHO"
+let lastName = "MYUNG"
+
+let greeting = "Hello, Playground"
+let album: String = "Reputation"
+let year: Int = 2021
+let height: Double = 1.82
+let haveBabyBody: Bool = true
