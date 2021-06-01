0dfee54a4b023dfe401a091af3a9bbe5a71f6128
jinho6225 <jinho6225@hotmail.com>
2021-05-31 20:41:15 -0700

    studied classes

---
+++ Udemy-iOSAndSwift/projects/ClassesDemo/ClassesDemo/Enemy.swift
@@ -0,0 +1,13 @@
+
+class Enemy {
+    var health = 100
+    var attackStrength = 10
+    
+    func move() {
+        print("Walk forward.")
+    }
+    
+    func attack() {
+        print("Land a hit, does \(attackStrength) damage.")
+    }
+}
